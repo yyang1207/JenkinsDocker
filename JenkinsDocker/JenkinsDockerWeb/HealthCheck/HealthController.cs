314f7d9f3e45671bf1c15afb0459e038fc670ab6
yyang1207 <yyang_1207@163.com>
2020-07-17 10:53:02 +0800

    健康检查

---
+++ JenkinsDocker/JenkinsDockerWeb/HealthCheck/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsDockerWeb.HealthCheck
+{
+    [Route("api/Health")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("ok");
+    }
+}
